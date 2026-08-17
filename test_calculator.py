e39d71d5d49490cd33ae40a1bc6b24f8c8f11bf8
Sai Pranav <pullanollasaipranav@gmail.com>
2026-08-17 18:47:35 +0530

    Add subtract function

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+from calculator import add, multiply, subtract
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_subtract():
+    assert subtract(5, 2) == 3
